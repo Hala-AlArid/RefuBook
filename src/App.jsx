53ff4ee801151a3f949260769c3629d235aad0ee
Hala AlArid <halaalarid99@gmail.com>
2022-12-19 22:59:40 +0200

    added files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import SignUp from "./pages/SignUp/SignUp";
+import SignIn from "./pages/SignIn/SignIn";
+import Contact from "./pages/Contact/Contact";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Profile from "./pages/Profile/Profile";
 
 function App() {
   return (
-    <div className="App">
-      <h3>Hello Callback Cats!</h3>
-    </div>
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="about" element={<About />} />
+    <Route path="signup" element={<SignUp />} />
+    <Route path="signin" element={<SignIn />} />
+    <Route path="contact" element={<Contact />} />
+    <Route path="dashboard" element={<Dashboard />} />
+    <Route path="profile" element={<Profile />} />
+  </Routes>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-// import { Provider } from 'react-redux';
+import { BrowserRouter } from "react-router-dom";
 import App from './App';
 import './index.css';
 
@@ -9,8 +9,8 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    {/* <Provider> */}
-      <App />
-    {/* </Provider> */}
+    <BrowserRouter>
+    <App/>
+    </BrowserRouter>
   </React.StrictMode>
 );
