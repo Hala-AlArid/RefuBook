85fb035d765694f004fe2465547531472fff1afb
Maria Dayekh <mariaaldayekh@gmail.com>
2022-12-21 15:36:13 +0200

    fixed layout

--- src/components/Stories/Stories.js
+++ src/components/Stories/Stories.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import "../../App.css";
+import "./Stories.css";
 import Image from"../../images/Image.png";
 
 function Stories (){
-   return <div className='background w-full min-h-fit pl-20 pt-20' >
+   return <div className='background w-full min-h-fit pl-20 pt-20 relative' >
     { /* SUBTITLE */}
-    <div>
+    <div className='inline-block relative'>
         <h2 className='text-white text-6xl'>
             Latest Stories</h2>
         <p className='text-xl text-white pt-4 max-w-lg'>
@@ -13,17 +13,20 @@ function Stories (){
     </div>
 
     { /* STORIES */ }
-     <div className='flex justify-center grid grid-cols-2 gap-x-32 pt-16 flex align-center items-center place-content-stretch pb-10'>
+     <div className='inline-block relative flex justify-center grid grid-cols-2 gap-x-32 pt-16 flex align-center items-center place-content-stretch pb-10'>
         { /* STORY ROW */ }
        <div className='w-max  ml-0 '><img className='w-fit' alt='article' src={Image}/></div>
 
        <div className='grid grid-cols-1 w-96 '>
-        <p className='bg-white langcolor p-2 w-24 rounded-md font-bold pl-3 align-center'>Language</p>
-        <p className='text-white text-4xl pt-2 pb-2 font-thin leading-normal'>
+        <div className='w-fit '>
+         <p className='p-2 w-full rounded-md font-bold  align-center lang bg-white text-sky-600 text-xs	'>
+            Language</p>
+        </div>
+            <p className='text-white text-4xl pt-2 pb-2 font-thin leading-normal'>
             How I learned Turkish quickly and what are the best places to learn.</p>
-        <div className='pb-8 pt-4 grid grid-cols-2 items-center text-white gap-x-0.5'>
+        <div className='pb-8 pt-4 grid grid-cols-2 items-center text-white gap-x-0.5 w-7/12'>
             <div>profile pic</div>
-            <div>
+            <div className='w-full'>
                 <p className='font-bold'>Ahmad Faysal</p>
                 <p className='font-thin'>Refugee in Turkey</p>
             </div>
