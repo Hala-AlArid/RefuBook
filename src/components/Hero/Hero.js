764a71c0bd083a5582abbbf67d1c224b8f75cf2d
Maria Dayekh <mariaaldayekh@gmail.com>
2022-12-23 21:21:10 +0200

    Styled hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -10,7 +10,7 @@ function Hero (){
             <h1 className="text-7xl font-black refubook pb-2">Refubook</h1>
             <h2 className="text-6xl font-medium express pb-6 w-60 text-rose-300">Express Freely</h2>
             <p className="text-base font-light w-80">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. </p>
-            <div className="button w-36 bg-sky-600 rounded-3xl mt-8 mb-10	"><p className="text-xl font-bold py-2 text-white text-center">Sign up</p></div>
+            <button type="button" className="button w-36 bg-sky-600 rounded-full mt-8 mb-10	"><p className="text-xl font-bold pb-3 pt-2 text-white text-center">Sign up</p></button>
         </div>
         <div>
             <img src={homepage} alt="home page graphic"/>
