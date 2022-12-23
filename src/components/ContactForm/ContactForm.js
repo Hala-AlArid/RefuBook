2ce98d4ae90f0b776b3f51ca5bf77d8699b0d99a
ayamarmash <ayamarmash74@gmail.com>
2022-12-23 05:25:13 +0200

    Contact Page finished, Navbar remaining

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BsTwitter , BsInstagram} from 'react-icons/bs';
+import { BsTwitter, BsInstagram } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
 import Img from './FormImg.png';
 import './ContactForm.css';
@@ -7,6 +7,7 @@ import './ContactForm.css';
 function ContactForm() {
   return (
     <div className="FormBody">
+      <img src={Img} alt="illustration" />
       <div className="Content">
         <h1>Get in Touch</h1>
         <h3>
@@ -14,19 +15,18 @@ function ContactForm() {
           with the introduction.
         </h3>
         <br />
-        <form id='form'>
+        <form id="form">
           <h3>Email</h3>
           <input id="email" type="text" placeholder="    hello@gmail.com" />
           <input id="message" type="text" placeholder="    Message" />
-          <input id="submit" type="submit" value="Send"/>
+          <input id="submit" type="submit" value="Send" />
         </form>
-        <div className='IconsList'>
-        <BsTwitter className='Icon'/>
-        <BsInstagram className='Icon'/>
-        <FaFacebookF className='Icon'/>
+        <div className="IconsList">
+          <BsTwitter className="Icon" />
+          <BsInstagram className="Icon" />
+          <FaFacebookF className="Icon" />
         </div>
       </div>
-      <img src={Img} alt="illustration" />
     </div>
   );
 }
