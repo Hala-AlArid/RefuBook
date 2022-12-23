260201de6c39cdfba44918e700e1fd15120d6467
ayamarmash <ayamarmash74@gmail.com>
2022-12-23 03:03:10 +0200

    initial commit

---
+++ src/pages/Contact/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContactForm from '../../components/ContactForm/ContactForm';
+import './Contact.css';
+
+function Contact() {
+  return (
+    <div className="Page">
+      <ContactForm id="Form"/>
+    </div>
+  );
+}
+export default Contact;
