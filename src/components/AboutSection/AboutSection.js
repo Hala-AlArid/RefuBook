61f20cf6f22aa4b8d5903dfc7b8e4db789020684
Hala AlArid <halaalarid99@gmail.com>
2022-12-24 16:11:40 +0200

    Create and Style AboutSection Fixes #3

--- src/components/AboutSection/AboutSection.js
+++ src/components/AboutSection/AboutSection.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import "./AboutSection.css"
 
 function AboutSection(){
     return (
-        <div></div>
+        <div className='AboutSection'>
+            <h1><b>About</b></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod rerum ipsa dolorem maxime illo, 
+                sequi adipisci non eaque quis quas? Minima aut ab commodi veritatis libero? Neque eaque aliquam dolores.</p>
+            <button type='submit'>Learn more</button>
+        </div>
     );
 }
 
