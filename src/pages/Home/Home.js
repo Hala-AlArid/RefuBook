7196150f63d0f57779ed4915ed18cbed273c148d
Maria Dayekh <mariaaldayekh@gmail.com>
2022-12-26 15:04:52 +0200

    Styled partners

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Partners from "../../components/Partners/Partners";
+
+function Home (){
+    return <div>
+        <Partners />
+    </div>
+};
+
+export default Home;
