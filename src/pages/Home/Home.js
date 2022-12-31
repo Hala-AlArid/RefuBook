0f1cb1b1f990193e79682ff4731595260c2fade8
Batoul El Mansour <elmansourbatoul@gmail.com>
2022-12-31 22:26:46 +0200

    Create and Style Features Fixes #5

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Testimonial from '../../components/Testimonials/Testimonials';
+const Home = () => {
+  return (
+    <>
+      <Testimonial />
+    </>
+  );
+};
+
+export default Home;
