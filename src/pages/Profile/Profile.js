55399f8148ac2ef8bb52ef7e5b5028b3a3ccdd98
Hala AlArid <halaalarid99@gmail.com>
2023-01-22 22:48:53 +0200

    Merged NavBar2.js

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom';
-
+import NavBar2 from "../../components/NavBar/NavBar2";
 function Profile(){
 
     const navigate = useNavigate();
@@ -12,6 +12,7 @@ function Profile(){
 
     return (
         <div>
+            <NavBar2/>
             <h3>welcome {localStorage.getItem("email")}</h3>
             <button type="submit" onClick={logout}>logout</button>
             </div>
